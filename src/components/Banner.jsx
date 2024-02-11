413861c08de126851d04b8a966793c86609833b5
Nicolas <contact@nicolasrichelet.com>
2024-02-11 16:49:08 +0100

    Mis en place de la "Banner" avec l'image qui s'adapte en fonction de la page. (URL)

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,16 +1,48 @@
+import React from 'react'
 import coast from '../assets/banner/coast.jpg'
+import mountains from '../assets/banner/mountains.jpg'
 
-function Banner() {
+import { useLocation } from 'react-router-dom'
+
+// get our fontawesome imports
+
+const Banner = () => {
+  const location = useLocation()
+
+  // Function to determine which image to render based on the URL
+  const getImageForURL = () => {
+    return location.pathname.includes('About') ? mountains : coast
+  }
+
+  const getTitleForURL = () => {
+    return location.pathname.includes('About')
+      ? ''
+      : 'Chez vous, partout et ailleurs!'
+  }
+
   return (
     <div className="banner">
       <img
         className="banner__img"
-        src={coast}
+        src={getImageForURL()}
         alt="un bord de mer agité et montagneux"
       />
-      <h2 className="banner__title">Chez vous, partout et ailleurs</h2>
+      <h2 className="banner__title">{getTitleForURL()}</h2>
     </div>
   )
 }
+
+// function Banner() {
+//   return (
+//     <div className="banner">
+//       <img
+//         className="banner__img"
+//         src={coast}
+//         alt="un bord de mer agité et montagneux"
+//       />
+//       <h2 className="banner__title">Chez vous, partout et ailleurs</h2>
+//     </div>
+//   )
+// }
 
 export default Banner
