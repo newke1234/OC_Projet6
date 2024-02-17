ef0461a0e81b6cf688b443c79a77ecb8eb9d45ce
Nicolas <contact@nicolasrichelet.com>
2024-02-17 17:29:14 +0100

    Ajout des liens vers la page du logement selectionné. Page "House"

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Error from './pages/404/404'
 import About from './pages/About/About'
+import House from './pages/House/House'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import './styles/index.scss'
@@ -18,6 +19,7 @@ root.render(
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="*" element={<Error />} />
+        <Route path="/house/:id" element={<House />} />
       </Routes>
       <Footer />
     </Router>
