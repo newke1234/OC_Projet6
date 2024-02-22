69deb1d0f84972ba782c0803a8508f5caf445830
Nicolas <contact@nicolasrichelet.com>
2024-02-22 23:00:54 +0100

    MAJ de la droddownbox

--- src/components/DropDownBox.jsx
+++ src/components/DropDownBox.jsx
@@ -1,51 +1,79 @@
 import React from 'react'
 import { useState } from 'react'
-import jsonData from '../datas/about-boxes.json'
 import '../styles/dropdown.scss'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
-function AnimatedBoxGroup() {
-  const [openBoxes, setOpenBoxes] = useState([])
+function DropDownBox({ title, content }) {
+  const [isOpen, setIsOpen] = useState(false)
 
-  const toggleBox = (boxId) => {
-    setOpenBoxes((prevOpenBoxes) => {
-      if (prevOpenBoxes.includes(boxId)) {
-        return prevOpenBoxes.filter((id) => id !== boxId)
-      } else {
-        return [...prevOpenBoxes, boxId]
-      }
-    })
-  }
-
-  const isBoxOpen = (boxId) => {
-    return openBoxes.includes(boxId)
+  const toggleBox = () => {
+    setIsOpen((prevIsOpen) => !prevIsOpen)
   }
 
   return (
-    <div className="animated-box-group">
-      {jsonData.map((box, index) => (
-        <div
-          key={index}
-          className={`box ${isBoxOpen(index) ? 'open' : ''}`}
-          onClick={() => toggleBox(index)}
-        >
-          <div className="box__title">
-            <div>{box.boxtitle}</div>
-            <div>
-              <FontAwesomeIcon
-                icon={isBoxOpen(index) ? faChevronDown : faChevronUp}
-              />
-            </div>
-          </div>
-          <div className="box__text-container">
-            <div className="box__text">{box.boxtext}</div>
-          </div>
+    <div className={`box ${isOpen ? 'open' : ''}`}>
+      <div className="box__title" onClick={toggleBox}>
+        <div>{title}</div>
+        <div>
+          <FontAwesomeIcon icon={isOpen ? faChevronDown : faChevronUp} />
         </div>
-      ))}
+      </div>
+      <div className="box__text-container">
+        {isOpen &&
+          content.map((item, index) => (
+            <div key={index} className="box__text">
+              {item}
+            </div>
+          ))}
+      </div>
     </div>
   )
 }
 
-export default AnimatedBoxGroup
+export default DropDownBox
+
+// function AnimatedBoxGroup() {
+//   const [openBoxes, setOpenBoxes] = useState([])
+
+//   const toggleBox = (boxId) => {
+//     setOpenBoxes((prevOpenBoxes) => {
+//       if (prevOpenBoxes.includes(boxId)) {
+//         return prevOpenBoxes.filter((id) => id !== boxId)
+//       } else {
+//         return [...prevOpenBoxes, boxId]
+//       }
+//     })
+//   }
+
+//   const isBoxOpen = (boxId) => {
+//     return openBoxes.includes(boxId)
+//   }
+
+//   return (
+//     <div className="animated-box-group">
+//       {jsonData.map((box, index) => (
+//         <div
+//           key={index}
+//           className={`box ${isBoxOpen(index) ? 'open' : ''}`}
+//           onClick={() => toggleBox(index)}
+//         >
+//           <div className="box__title">
+//             <div>{box.boxtitle}</div>
+//             <div>
+//               <FontAwesomeIcon
+//                 icon={isBoxOpen(index) ? faChevronDown : faChevronUp}
+//               />
+//             </div>
+//           </div>
+//           <div className="box__text-container">
+//             <div className="box__text">{box.boxtext}</div>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   )
+// }
+
+// export default AnimatedBoxGroup
