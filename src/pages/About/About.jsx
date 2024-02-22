969e284ccc830ec3c5e2dde50552f784cedc381c
Nicolas <contact@nicolasrichelet.com>
2024-02-22 23:02:19 +0100

    Maj avec des props pour la dropDownBox

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,14 +1,39 @@
+import React from 'react'
 import Banner from '../../components/Banner'
 import DropDownBox from '../../components/DropDownBox'
+import jsonData from '../../datas/about-boxes.json'
 import '../../styles/dropdown.scss'
 
 function About() {
   return (
     <div>
       <Banner />
-      <DropDownBox />
+      <div className="animated-box-group">
+        {jsonData.map((box, index) => (
+          <DropDownBox
+            key={index}
+            title={box.boxtitle}
+            content={[box.boxtext]}
+          />
+        ))}
+      </div>
     </div>
   )
 }
 
 export default About
+
+// import Banner from '../../components/Banner'
+// import DropDownBox from '../../components/DropDownBox'
+// import '../../styles/dropdown.scss'
+
+// function About() {
+//   return (
+//     <div>
+//       <Banner />
+//       <DropDownBox />
+//     </div>
+//   )
+// }
+
+// export default About
